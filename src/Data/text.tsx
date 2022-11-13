cab453d19ecea392705c1ff8ba28e2b7bfb0633a
Wayne Wang <sw@middlebury.edu>
2022-11-12 23:34:49 -0500

    text change

--- src/Data/text.tsx
+++ src/Data/text.tsx
@@ -7,8 +7,8 @@ At Middlebury College, I'm studying Computer Science and Political Science.
 
 export const moreText = `
 I'm a *Computer Science* major super senior-Feb (aka. one semester left) at [Middlebury College](https://www.middlebury.edu/).
-I love exploring and researching *operating system kernels*, security, networking, and other tech infrastructures, as the ins-and-outs of systems fascinate me.
-My concern about how tech impacts society motivates me to build *human-centered* and elegant front-end experiences.
+I love exploring and researching *operating system kernels*, security, networks, and other tech infrastructures, as the ins-and-outs of systems fascinate me.
+My concern about how tech impacts everyday life motivates me to build *human-centered* and elegant front-end experiences.
 <br />
 Outside of tech, I am also interested in the intricacies of political systems and societies, as I also minor in *Political Science*.
-I'm fortunate to enjoy the companion of two *cats* back at home, *六六 (Liu-Liu)* and *小七 (Xiao-Qi)*, who will be thrilled if you could pay a visit by clicking the button at the bottom right corner :D`;
+I'm fortunate to enjoy the companion of two *cats* at home, *六六 (Liu-Liu)* and *小七 (Xiao-Qi)*, who will be thrilled if you could pay a visit by clicking the button at the bottom right corner :D`;
